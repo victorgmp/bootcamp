3c502652a477d96665457ed1b74fafd0127396f2
Víctor Moreno <victorgmp@gmail.com>
2021-03-31 22:22:59 -0400

    add heroku config

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 // app.use(morgan('tiny'))
 
@@ -101,7 +104,7 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
