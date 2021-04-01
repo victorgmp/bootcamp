9f34dc5eeee0e1dbe8433a051ed44e2ef97703de
Víctor Moreno <victorgmp@gmail.com>
2021-04-01 11:38:15 -0400

    add front build

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,6 +8,8 @@ app.use(cors())
 app.use(express.json())
 // app.use(morgan('tiny'))
 
+app.use(express.static('build'))
+
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body - :req[content-length]'))
 
